5cdf12d53949331b9b6be5b2527b12d96c4004da
AssADev <gravelteo.pro@gmail.com>
2026-01-01 16:22:22 +0100

    feat: Service modules (Hero, Offers & FAQ) (WIP)

---
+++ src/storyblok/modules/service/ServiceFAQ.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { StoryblokServiceFAQ } from '#types/component-types-sb.js';
+
+// Props :
+defineProps<{
+	blok: StoryblokServiceFAQ;
+}>();
+</script>
+
+<template><div>ServiceFAQ</div></template>
+
+<style lang="scss" scoped>
+div {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 60vh;
+	border: 1px solid red;
+}
+</style>
---
+++ src/storyblok/modules/service/ServiceHero.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { StoryblokRichText, type StoryblokRichTextNode } from '@storyblok/vue';
+import { type VNode, h } from 'vue';
+
+import { nl2br } from '#utils/nl2br.ts';
+
+import RichText from '#components/utils/RichText.vue';
+
+import type { StoryblokServiceInformations } from '#types/component-types-sb.js';
+
+// Props :
+defineProps<{
+	blok: StoryblokServiceInformations;
+}>();
+
+// Resolvers (RichText) :
+const resolvers = {
+	paragraph: (node: StoryblokRichTextNode<VNode>) =>
+		h('h1', h(StoryblokRichText, { doc: { type: 'doc', content: node.content } }))
+};
+</script>
+
+<template>
+	<section class="modules service-hero">
+		<div class="container-grid">
+			<p
+				class="summary col-start-3 col-end-13 col-start-tb-11 col-end-tb-15 col-start-dk-20 col-end-dk-28 col-start-lg-20 col-end-lg-27 col-start-xlg-20 col-end-xlg-26 col-start-xxlg-20 col-end-xxlg-25"
+				v-html="nl2br(blok.summary)"
+			></p>
+		</div>
+		<div class="container">
+			<RichText :doc="blok.name" :resolvers="resolvers" />
+		</div>
+	</section>
+</template>
+
+<style lang="scss" scoped>
+.service-hero {
+	padding-block: fluidSize(120px, 100px) fluidSize(60px, 40px);
+}
+
+:deep(.partials-rich-text) {
+	@include roobert-96;
+
+	em {
+		@include romie-96-italic;
+	}
+}
+
+.summary {
+	@include roobert-14;
+
+	max-width: fluidSize(420px, 380px);
+	margin-block-end: fluidSize(60px, 45px);
+}
+</style>
