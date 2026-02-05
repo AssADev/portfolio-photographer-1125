e3d89ec97415d55da57b01ce45698e6cc42b5fc6
AssADev <gravelteo.pro@gmail.com>
2026-02-05 18:21:09 +0100

    feat: homeLayouts responsive added

--- src/utils/homeLayouts.ts
+++ src/utils/homeLayouts.ts
@@ -1,40 +1,242 @@
 export const HOME_LAYOUT_CONFIGS: Record<string, string[][]> = {
 	'layout-one': [
-		['col-start-1', 'col-end-10', 'col-start-dk-3', 'col-end-dk-16'],
-		['col-start-5', 'col-end-12', 'col-start-dk-22', 'col-end-dk-30'],
-		['col-start-5', 'col-end-12', 'col-start-dk-10', 'col-end-dk-21']
+		[
+			'col-start-1',
+			'col-end-13',
+			'col-start-dk-3',
+			'col-end-dk-16',
+			'col-start-lg-3',
+			'col-end-lg-17',
+			'col-start-xxlg-3',
+			'col-end-xxlg-18'
+		],
+		[
+			'col-start-2',
+			'col-end-10',
+			'col-start-dk-22',
+			'col-end-dk-30',
+			'col-start-lg-21',
+			'col-end-lg-30',
+			'col-start-xxlg-21',
+			'col-end-xxlg-31'
+		],
+		[
+			'col-start-3',
+			'col-end-12',
+			'col-start-dk-10',
+			'col-end-dk-21',
+			'col-start-lg-10',
+			'col-end-lg-22',
+			'col-start-xxlg-10',
+			'col-end-xxlg-23'
+		]
 	],
 	'layout-two': [
-		['col-start-1', 'col-end-13', 'col-start-dk-1', 'col-end-dk-16'],
-		['col-start-1', 'col-end-13', 'col-start-dk-22', 'col-end-dk-31']
+		[
+			'col-start-1',
+			'col-end-13',
+			'col-start-dk-1',
+			'col-end-dk-16',
+			'col-start-lg-1',
+			'col-end-lg-17',
+			'col-start-xxlg-1',
+			'col-end-xxlg-18'
+		],
+		[
+			'col-start-5',
+			'col-end-13',
+			'col-start-dk-22',
+			'col-end-dk-31',
+			'col-start-lg-21',
+			'col-end-lg-31',
+			'col-start-xxlg-20',
+			'col-end-xxlg-31'
+		]
 	],
 	'layout-three': [
-		['col-start-2', 'col-end-9', 'col-start-dk-11', 'col-end-dk-18'],
-		['col-start-4', 'col-end-13', 'col-start-dk-3', 'col-end-dk-10'],
-		['col-start-4', 'col-end-13', 'col-start-dk-17', 'col-end-dk-30']
+		[
+			'col-start-2',
+			'col-end-11',
+			'col-start-dk-11',
+			'col-end-dk-18',
+			'col-start-lg-11',
+			'col-end-lg-19',
+			'col-start-xxlg-11',
+			'col-end-xxlg-20'
+		],
+		[
+			'col-start-1',
+			'col-end-8',
+			'col-start-dk-3',
+			'col-end-dk-10',
+			'col-start-lg-3',
+			'col-end-lg-11',
+			'col-start-xxlg-2',
+			'col-end-xxlg-11'
+		],
+		[
+			'col-start-4',
+			'col-end-13',
+			'col-start-dk-17',
+			'col-end-dk-30',
+			'col-start-lg-17',
+			'col-end-lg-31',
+			'col-start-xxlg-16',
+			'col-end-xxlg-31'
+		]
 	],
 	'layout-four': [
-		['col-start-1', 'col-end-12', 'col-start-dk-13', 'col-end-dk-33'],
-		['col-start-1', 'col-end-12', 'col-start-dk-2', 'col-end-dk-11']
+		[
+			'col-start-1',
+			'col-end-13',
+			'col-start-dk-13',
+			'col-end-dk-33',
+			'col-start-lg-12',
+			'col-end-lg-33',
+			'col-start-xxlg-11',
+			'col-end-xxlg-33'
+		],
+		[
+			'col-start-1',
+			'col-end-12',
+			'col-start-dk-2',
+			'col-end-dk-11',
+			'col-start-lg-2',
+			'col-end-lg-12',
+			'col-start-xxlg-2',
+			'col-end-xxlg-13'
+		]
 	],
-	'layout-five': [['col-start-4', 'col-end-13', 'col-start-dk-8', 'col-end-dk-29']],
+	'layout-five': [
+		[
+			'col-start-3',
+			'col-end-13',
+			'col-start-dk-8',
+			'col-end-dk-29',
+			'col-start-lg-8',
+			'col-end-lg-30',
+			'col-start-xxlg-7',
+			'col-end-xxlg-30'
+		]
+	],
 	'layout-six': [
-		['col-start-3', 'col-end-13', 'col-start-dk-4', 'col-end-dk-14'],
-		['col-start-1', 'col-end-7', 'col-start-dk-16', 'col-end-dk-30'],
-		['col-start-1', 'col-end-7', 'col-start-dk-5', 'col-end-dk-12']
+		[
+			'col-start-1',
+			'col-end-10',
+			'col-start-dk-4',
+			'col-end-dk-14',
+			'col-start-lg-3',
+			'col-end-lg-14',
+			'col-start-xxlg-3',
+			'col-end-xxlg-15'
+		],
+		[
+			'col-start-4',
+			'col-end-12',
+			'col-start-dk-16',
+			'col-end-dk-30',
+			'col-start-lg-15',
+			'col-end-lg-30',
+			'col-start-xxlg-15',
+			'col-end-xxlg-31'
+		],
+		[
+			'col-start-2',
+			'col-end-12',
+			'col-start-dk-5',
+			'col-end-dk-12',
+			'col-start-lg-5',
+			'col-end-lg-13',
+			'col-start-xxlg-5',
+			'col-end-xxlg-14'
+		]
 	],
-	'layout-seven': [['col-start-5', 'col-end-12', 'col-start-dk-2', 'col-end-dk-28']],
+	'layout-seven': [
+		[
+			'col-start-1',
+			'col-end-13',
+			'col-start-dk-2',
+			'col-end-dk-28',
+			'col-start-lg-2',
+			'col-end-lg-29',
+			'col-start-xxlg-1',
+			'col-end-xxlg-29'
+		]
+	],
 	'layout-eight': [
-		['col-start-5', 'col-end-13', 'col-start-dk-5', 'col-end-dk-18'],
-		['col-start-1', 'col-end-11', 'col-start-dk-25', 'col-end-dk-32']
+		[
+			'col-start-2',
+			'col-end-11',
+			'col-start-dk-5',
+			'col-end-dk-18',
+			'col-start-lg-5',
+			'col-end-lg-19',
+			'col-start-xxlg-5',
+			'col-end-xxlg-20'
+		],
+		[
+			'col-start-6',
+			'col-end-13',
+			'col-start-dk-25',
+			'col-end-dk-32',
+			'col-start-lg-24',
+			'col-end-lg-32',
+			'col-start-xxlg-23',
+			'col-end-xxlg-32'
+		]
 	],
 	'layout-nine': [
-		['col-start-1', 'col-end-12', 'col-start-dk-14', 'col-end-dk-28'],
-		['col-start-7', 'col-end-13', 'col-start-dk-1', 'col-end-dk-9'],
-		['col-start-3', 'col-end-11', 'col-start-dk-13', 'col-end-dk-24']
+		[
+			'col-start-1',
+			'col-end-12',
+			'col-start-dk-14',
+			'col-end-dk-28',
+			'col-start-lg-13',
+			'col-end-lg-28',
+			'col-start-xxlg-13',
+			'col-end-xxlg-29'
+		],
+		[
+			'col-start-2',
+			'col-end-9',
+			'col-start-dk-1',
+			'col-end-dk-9',
+			'col-start-lg-1',
+			'col-end-lg-10',
+			'col-start-xxlg-1',
+			'col-end-xxlg-11'
+		],
+		[
+			'col-start-3',
+			'col-end-12',
+			'col-start-dk-13',
+			'col-end-dk-24',
+			'col-start-lg-12',
+			'col-end-lg-24',
+			'col-start-xxlg-12',
+			'col-end-xxlg-25'
+		]
 	],
 	'layout-ten': [
-		['col-start-1', 'col-end-13', 'col-start-dk-3', 'col-end-dk-32'],
-		['col-start-1', 'col-end-13', 'col-start-dk-8', 'col-end-dk-16']
+		[
+			'col-start-1',
+			'col-end-13',
+			'col-start-dk-3',
+			'col-end-dk-32',
+			'col-start-lg-2',
+			'col-end-lg-32',
+			'col-start-xxlg-2',
+			'col-end-xxlg-32'
+		],
+		[
+			'col-start-3',
+			'col-end-12',
+			'col-start-dk-8',
+			'col-end-dk-16',
+			'col-start-lg-8',
+			'col-end-lg-17',
+			'col-start-xxlg-8',
+			'col-end-xxlg-18'
+		]
 	]
 };
